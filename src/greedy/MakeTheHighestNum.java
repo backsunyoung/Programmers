3256ae863e47f4b4d5ad6a9f5db56ce6302b365b
bagsunyoung <malang333@naver.com>
2025-09-20 18:39:17 +0900

    그리디 알고리즘 - 가장 큰 수 구하기 문제

--- src/greedy/MakeTheHighestNum.java
+++ src/greedy/MakeTheHighestNum.java
@@ -5,14 +5,52 @@ public class MakeTheHighestNum {
     public class Solution {
         public String solution(String number, int k) {
 
-            StringBuilder n = new StringBuilder(number);
+            String answer = "";
 
-            for(int i = 0; i < k; i++){
+
+            StringBuilder s = new StringBuilder();
+
+            for(int i = 0; i < number.length(); i++){
+
+                char c = number.charAt(i);
+
+                while (k > 0 && s.length() > 0 && s.charAt(s.length()-1) < c){
+                    s.deleteCharAt(s.length() -1);
+                    k--;
+
+                    System.out.println(s);
+                }
+                s.append(c);
+
+
+
+
+
+
+
+//                if(n.charAt(i) >= n.charAt(i+1)){
+//                    s.append(n.charAt(i));
+//                }
+//
+//                if(s.length() > number.length() - k){
+//                    break;
+//                }
+
+
+
+
 
 
             }
+            System.out.println("--------");
 
-            String answer = "";
+            if (k > 0){
+                System.out.println(s);
+                s.setLength(s.length() - k);
+            }
+
+            answer = s.toString();
+
             return answer;
         }
     }
